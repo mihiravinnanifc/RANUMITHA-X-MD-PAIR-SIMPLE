dd72f287d886fd79b2f1586b61d508cda13ed4c7
mahirambalram <hirukaranumithagitbotadkh@gmail.com>
2026-02-07 14:13:03 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -13,7 +13,7 @@ function removeFile(FilePath) {
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-    async function DARK_KNIGHT_XMD_PAIR_CODE() {
+    async function RANUMITHA_X_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
@@ -75,36 +75,31 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "dark~" + string_session;
+                        let md = "ranu&" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hey there, 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳 user!* 👋🏻
+                        let desc = `*Hey there, RANUMITHA-X-MD user!* 👋🏻
 
-Thanks for using *𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳* — your session has been successfully created!
+✅ Your session has been created successfully.
 
 🔐 *Session ID:* Sent above  
-⚠️ *Keep it safe!* Do NOT share this ID with anyone.
+⚠️ *Do NOT share this ID*
 
 ——————
+📢 WhatsApp Channel  
+https://whatsapp.com/channel/0029Vb75EZf4IBhE8l3yXQ0P
 
-*✅ Stay Updated:*  
-join our official whatsApp channel:  
-https://whatsapp.com/channel/0029VbAM4eo3AzNQZ1WleW3e
-
-*💻 Support Group:*
-join our official whatsApp group:
-https://chat.whatsapp.com/IGgPW6pTrH14oAWCJALYR5?mode=ac_c
+💬 Support Group  
+https://chat.whatsapp.com/JNATLE4Sywc7XWJb7Wh8ka
 
 ——————
-
-> *© Powered by 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳*
-Stay cool and hack smart. ✌🏻`; 
+> © Powered by 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗 🌛`; 
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳",
-thumbnailUrl: "https://files.catbox.moe/brlkte.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029VbAM4eo3AzNQZ1WleW3e",
+title: "RANUMITHA-X-MD",
+thumbnailUrl: "https://raw.githubusercontent.com/Ranumithaofc/RANU-FILE-S-/refs/heads/main/images/IMG-20250711-WA0010.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029Vb75EZf4IBhE8l3yXQ0P",
 mediaType: 1,
 renderLargerThumbnail: true
 }  
@@ -113,34 +108,29 @@ renderLargerThumbnail: true
 {quoted:code })
                     } catch (e) {
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
-                            let desc = `Hey there, 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳 user!* 👋🏻
+                            let desc = `*Hey there, RANUMITHA-X-MD user!* 👋🏻
 
-Thanks for using *𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳* — your session has been successfully created!
+✅ Your session has been created successfully.
 
 🔐 *Session ID:* Sent above  
-⚠️ *Keep it safe!* Do NOT share this ID with anyone.
+⚠️ *Do NOT share this ID*
 
 ——————
+📢 WhatsApp Channel  
+https://whatsapp.com/channel/0029Vb75EZf4IBhE8l3yXQ0P
 
-*✅ Stay Updated:*  
-join our official whatsApp channel:  
-https://whatsapp.com/channel/0029VbAM4eo3AzNQZ1WleW3e
-
-*💻 Support Group:*  
-join our official whatsApp group: 
-https://chat.whatsapp.com/IGgPW6pTrH14oAWCJALYR5?mode=ac_c
+💬 Support Group  
+https://chat.whatsapp.com/JNATLE4Sywc7XWJb7Wh8ka
 
 ——————
-
-> *© Powered by 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳*
-Stay cool and hack smart. ✌🏻`;
+> © Powered by 𝗥𝗔𝗡𝗨𝗠𝗜𝗧𝗛𝗔-𝗫-𝗠𝗗 🌛`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳",
-thumbnailUrl: "https://files.catbox.moe/brlkte.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029VbAM4eo3AzNQZ1WleW3e",
+title: "RANUMITHA-X-MD",
+thumbnailUrl: "https://raw.githubusercontent.com/Ranumithaofc/RANU-FILE-S-/refs/heads/main/images/IMG-20250711-WA0010.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029Vb75EZf4IBhE8l3yXQ0P",
 mediaType: 2,
 renderLargerThumbnail: true,
 showAdAttribution: true
@@ -157,7 +147,7 @@ showAdAttribution: true
                     process.exit();
                 } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10);
-                    DARK_KNIGHT_XMD_PAIR_CODE();
+                    RANUMITHA_X_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -168,7 +158,7 @@ showAdAttribution: true
             }
         }
     }
-   return await DARK_KNIGHT_XMD_PAIR_CODE();
+   return await RANUMITHA_X_MD_PAIR_CODE();
 });/*
 setInterval(() => {
     console.log("☘️ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...");
